1c29ac06b62b82c73f19d12ae97c29adb132e204
fernandorego <fernandoluisrego@gmail.com>
2023-03-13 11:17:11 +0000

    Change token header to authorization header using Bearer Token

--- server/middlewares/authMiddleware.go
+++ server/middlewares/authMiddleware.go
@@ -4,16 +4,17 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"server/helpers"
+	"strings"
 )
 
 func AuthMiddleware(context *gin.Context) {
-	token := context.Request.Header.Get("access_token")
-	if token == "" {
+	authorization := strings.Split(context.Request.Header.Get("Authorization"), " ")
+	if len(authorization) != 2 || authorization[0] != "Bearer" {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": "token not provided"})
 		context.Abort()
 		return
 	}
-	claims, err := helpers.ValidateToken(token)
+	claims, err := helpers.ValidateToken(authorization[1])
 	if err != nil {
 		context.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 		context.Abort()
