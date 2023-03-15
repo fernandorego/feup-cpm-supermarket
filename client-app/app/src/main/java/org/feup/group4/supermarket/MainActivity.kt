5b875f32d1269528d8ac71d9d3579c6e23ec942f
fernandorego <fernandoluisrego@gmail.com>
2023-03-15 16:31:00 +0000

    Add login activity and icon

--- client-app/app/src/main/java/org/feup/group4/supermarket/MainActivity.kt
+++ client-app/app/src/main/java/org/feup/group4/supermarket/MainActivity.kt
@@ -1,11 +1,21 @@
 package org.feup.group4.supermarket
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val sharedPreferences = getSharedPreferences("MyToken", MODE_PRIVATE)
+        val token = sharedPreferences.getString("token", null)
+        if (token == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            return
+        }
+
         setContentView(R.layout.activity_main)
     }
 }
