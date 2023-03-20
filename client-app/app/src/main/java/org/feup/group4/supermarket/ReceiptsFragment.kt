b4a4761965c758bb08cf731605581ef47827ad72
GustavoASantos <gustavoamorimsantos10@gmail.com>
2023-03-20 16:24:13 +0000

    Improvements

---
+++ client-app/app/src/main/java/org/feup/group4/supermarket/ReceiptsFragment.kt
@@ -0,0 +1,18 @@
+package org.feup.group4.supermarket
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class ReceiptsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        return inflater.inflate(R.layout.fragment_receipts, container, false)
+    }
+}
