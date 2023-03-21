0bd1ac0659f3157903380991757dff64885756b5
GustavoASantos <gustavoamorimsantos10@gmail.com>
2023-03-21 22:59:54 +0000

    Add Coupons RecyclerView and Adapter

--- client-app/app/src/main/java/org/feup/group4/supermarket/HomeFragment.kt
+++ client-app/app/src/main/java/org/feup/group4/supermarket/HomeFragment.kt
@@ -4,16 +4,31 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-//        resources.getString(R.string.home_hello, )
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_home, container, false)
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val recyclerView = view.findViewById<RecyclerView>(R.id.home_coupons_list)
+        val emptyRecyclerView = view.findViewById<TextView>(R.id.empty_recyclerview)
+
+        if (coupons.isEmpty()) {
+            recyclerView.visibility = View.GONE
+            emptyRecyclerView.visibility = View.VISIBLE
+        } else {
+            recyclerView.visibility = View.VISIBLE
+            emptyRecyclerView.visibility = View.GONE
+        }
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        val adapter = CouponsAdapter(requireContext(), coupons)
+        recyclerView.adapter = adapter
     }
 }
