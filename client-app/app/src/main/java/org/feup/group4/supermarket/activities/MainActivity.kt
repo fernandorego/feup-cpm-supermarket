29abb51b5f6d2b7f8d12a21c9a3dd87ffca04514
GustavoASantos <gustavoamorimsantos10@gmail.com>
2023-03-22 18:54:07 +0000

    Remove launch activity

--- client-app/app/src/main/java/org/feup/group4/supermarket/activities/MainActivity.kt
+++ client-app/app/src/main/java/org/feup/group4/supermarket/activities/MainActivity.kt
@@ -1,22 +1,41 @@
 package org.feup.group4.supermarket.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.gson.Gson
 import org.feup.group4.supermarket.R
 import org.feup.group4.supermarket.databinding.ActivityMainBinding
+import org.feup.group4.supermarket.model.User
+import org.feup.group4.supermarket.service.UserService
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var user: User
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+
+        val token = UserService(this, null).getToken()
+        if (token == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        } else {
+            thread(start = true) {
+                UserService(this, ::afterHttpRequest).getUser()
+            }
+        }
 
         supportActionBar?.setDisplayShowCustomEnabled(true)
         supportActionBar?.setDisplayShowTitleEnabled(false)
@@ -38,7 +57,7 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        if ("org.feup.group4.supermarket.receipts" == intent.action) {
+        if (intent.action == "org.feup.group4.supermarket.receipts") {
             navView.selectedItemId = R.id.navigation_receipts
         }
     }
@@ -46,5 +65,30 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.activity_main, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun afterHttpRequest(statusCode: Int, json: String?) {
+        if (statusCode != 200) {
+            runOnUiThread {
+                Toast.makeText(
+                    applicationContext,
+                    "code: $statusCode\nmessage: $json",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+            return
+        }
+
+        this.user = Gson().fromJson(json, User::class.java)
+        runOnUiThread {
+            Toast.makeText(
+                applicationContext,
+                "email: ${this.user.email}\nname: ${this.user.name}\nuser_img: ${this.user.user_img}\n" +
+                        "is_admin: ${this.user.is_admin}",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
