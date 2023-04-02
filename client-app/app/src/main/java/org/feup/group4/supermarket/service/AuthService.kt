802a3825fbfcdbc00e01bd933018141524b5cfd6
fernandorego <fernandoluisrego@gmail.com>
2023-04-02 20:26:15 +0100

    Fix key pair generation

--- client-app/app/src/main/java/org/feup/group4/supermarket/service/AuthService.kt
+++ client-app/app/src/main/java/org/feup/group4/supermarket/service/AuthService.kt
@@ -1,32 +1,48 @@
 package org.feup.group4.supermarket.service
 
 import android.content.Context
+import android.security.KeyPairGeneratorSpec
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
 import org.feup.group4.supermarket.model.Card
 import org.feup.group4.supermarket.model.User
+import java.math.BigInteger
 import java.security.KeyPair
 import java.security.KeyPairGenerator
+import java.security.KeyStore
+import java.security.PublicKey
+import javax.security.auth.x500.X500Principal
+
+object CryptoConstants {
+    const val KEY_SIZE = 512
+    const val ANDROID_KEYSTORE = "AndroidKeyStore"
+    const val KEYNAME = "key_id"
+    const val SERIALNR = 1234567890L
+}
 
 class AuthService(context: Context, afterRequest: AfterRequest?) :
     HttpService(context, afterRequest) {
 
-    private fun generateKeyPair(): KeyPair {
-        val kpg: KeyPairGenerator = KeyPairGenerator.getInstance(
-            KeyProperties.KEY_ALGORITHM_RSA,
-            "AndroidKeyStore"
-        )
+    private fun generateKeyPair(): PublicKey {
         val parameterSpec: KeyGenParameterSpec = KeyGenParameterSpec.Builder(
-            "private_key",
+            CryptoConstants.KEYNAME,
             KeyProperties.PURPOSE_SIGN or KeyProperties.PURPOSE_VERIFY
         ).run {
-            setDigests(KeyProperties.DIGEST_SHA512)
+            setDigests(KeyProperties.DIGEST_SHA512, KeyProperties.DIGEST_SHA256)
+            setKeySize(CryptoConstants.KEY_SIZE)
+            setCertificateSerialNumber(BigInteger.valueOf(CryptoConstants.SERIALNR))
+            setCertificateSubject(X500Principal("CN=" + CryptoConstants.KEYNAME))
             build()
         }
-        kpg.initialize(parameterSpec)
-        return kpg.generateKeyPair()
+        KeyPairGenerator.getInstance(
+            KeyProperties.KEY_ALGORITHM_RSA,
+            CryptoConstants.ANDROID_KEYSTORE
+        ).run {
+            initialize(parameterSpec)
+            return generateKeyPair().public
+        }
     }
 
     fun setToken(token: String) {
@@ -40,22 +56,6 @@ class AuthService(context: Context, afterRequest: AfterRequest?) :
     fun login(nickname: String, password: String) =
         post("/getToken", Gson().toJson(User(nickname, password, null, null, null, null, null, null)))
 
-    fun register(name: String, nickname: String, password: String, card_number: String, card_cvv: String, card_date: String) {
-        val keypair = generateKeyPair()
-        post(
-            "/register",
-            Gson().toJson(
-                User(
-                    nickname,
-                    password,
-                    name,
-                    Card(card_number, card_cvv, card_date),
-                    keypair.public.encoded.toString(),
-                    null,
-                    null,
-                    null
-                )
-            )
-        )
-    }
+    fun register(name: String, nickname: String, password: String, card_number: String, card_cvv: String, card_date: String) =
+        post("/register", Gson().toJson(User(nickname, password, name, Card(card_number, card_cvv, card_date), generateKeyPair().encoded.toString(), null, null, null)))
 }
