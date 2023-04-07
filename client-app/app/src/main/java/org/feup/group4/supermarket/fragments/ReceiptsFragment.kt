f3be341ecefb79bb612495b8dfbe0755f651177c
GustavoASantos <gustavoamorimsantos10@gmail.com>
2023-04-07 17:00:53 +0100

    Add receipts adapter

--- client-app/app/src/main/java/org/feup/group4/supermarket/fragments/ReceiptsFragment.kt
+++ client-app/app/src/main/java/org/feup/group4/supermarket/fragments/ReceiptsFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import org.feup.group4.supermarket.R
+import org.feup.group4.supermarket.adapters.ReceiptsAdapter
+import org.feup.group4.supermarket.adapters.receipts
 
 class ReceiptsFragment : Fragment() {
     override fun onCreateView(
@@ -13,4 +18,23 @@ class ReceiptsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View = inflater.inflate(R.layout.fragment_receipts, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.receipts_list)
+        val emptyRecyclerView = view.findViewById<TextView>(R.id.empty_recyclerview)
+
+        if (receipts.isEmpty()) {
+            recyclerView.visibility = View.GONE
+            emptyRecyclerView.visibility = View.VISIBLE
+        } else {
+            recyclerView.visibility = View.VISIBLE
+            emptyRecyclerView.visibility = View.GONE
+        }
+
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        val adapter = ReceiptsAdapter(requireContext(), receipts)
+        recyclerView.adapter = adapter
+    }
 }
