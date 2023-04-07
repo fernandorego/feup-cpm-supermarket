66c968df18a5f8085c386eedf688b9882644771a
GustavoASantos <gustavoamorimsantos10@gmail.com>
2023-04-07 17:29:50 +0100

    Improve account layout

--- client-app/app/src/main/java/org/feup/group4/supermarket/activities/AccountActivity.kt
+++ client-app/app/src/main/java/org/feup/group4/supermarket/activities/AccountActivity.kt
@@ -3,17 +3,25 @@ package org.feup.group4.supermarket.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
 import android.widget.Button
+import android.widget.TextView
 import org.feup.group4.supermarket.R
+import org.feup.group4.supermarket.model.User
 import org.feup.group4.supermarket.service.AuthService
 
 class AccountActivity : AppCompatActivity() {
     private val logoutBtn: Button by lazy { findViewById(R.id.btn_logout) }
+    private val user: User = MainActivity.user
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account)
+
+        val accountNameTv: TextView = findViewById(R.id.account_name)
+        accountNameTv.text = getString(R.string.string_format, this.user.name)
+
+        val accountNickTv: TextView = findViewById(R.id.account_nick)
+        accountNickTv.text = getString(R.string.string_format, this.user.nickname)
     }
 
     override fun onStart() {
