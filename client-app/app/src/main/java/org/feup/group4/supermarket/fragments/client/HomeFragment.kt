403195fbbf558414a2d94ee9f09214ef2a888b3d
fernandorego <fernandoluisrego@gmail.com>
2023-04-14 13:04:10 +0100

    Add swipe to refresh user and coupons

--- client-app/app/src/main/java/org/feup/group4/supermarket/fragments/client/HomeFragment.kt
+++ client-app/app/src/main/java/org/feup/group4/supermarket/fragments/client/HomeFragment.kt
@@ -6,15 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.gson.Gson
 import org.feup.group4.supermarket.R
+import org.feup.group4.supermarket.activities.LoginActivity
+import org.feup.group4.supermarket.activities.MainActivity
 import org.feup.group4.supermarket.activities.client.ClientActivity
 import org.feup.group4.supermarket.activities.client.PurchaseActivity
+import org.feup.group4.supermarket.activities.terminal.TerminalActivity
 import org.feup.group4.supermarket.adapters.CouponsAdapter
 import org.feup.group4.supermarket.adapters.coupons
+import org.feup.group4.supermarket.model.Coupon
 import org.feup.group4.supermarket.model.User
+import org.feup.group4.supermarket.service.AuthService
+import org.feup.group4.supermarket.service.UserService
+import kotlin.concurrent.thread
 
 class HomeFragment : Fragment() {
     private val user: User = ClientActivity.user
@@ -58,5 +68,33 @@ class HomeFragment : Fragment() {
         newPurchaseButton.setOnClickListener {
             startActivity(Intent(activity, PurchaseActivity::class.java))
         }
+
+        val swipe:SwipeRefreshLayout = view.findViewById(R.id.swipe_refresh)
+        swipe.setOnRefreshListener {
+            thread(start = true) {
+                activity?.let { UserService(it.applicationContext, ::updateUser).getUser() }
+                swipe.isRefreshing = false
+            }
+        }
+    }
+
+    private fun updateUser(statusCode: Int, json: String?) {
+        if (statusCode != 200) {
+            activity?.runOnUiThread {
+                Toast.makeText(
+                    requireContext(),
+                    "code: $statusCode\nmessage: $json",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            activity?.startActivity(Intent(requireContext(), LoginActivity::class.java))
+            activity?.finish()
+            return
+        }
+
+        ClientActivity.user = Gson().fromJson(json, User::class.java)
+        val adapter = view?.findViewById<RecyclerView>(R.id.home_coupons_list)?.adapter as CouponsAdapter
+        adapter.setCoupons(ClientActivity.user.active_coupons as ArrayList<Coupon>)
+        adapter.notifyDataSetChanged()
     }
 }
