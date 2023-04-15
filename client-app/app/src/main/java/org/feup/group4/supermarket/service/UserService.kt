ce53a6b89cee924ea2194a8d7a85f6c47186680a
Bruno Mendes <bd_mendes@outlook.com>
2023-04-15 17:30:35 +0100

    Get user from db and use when offline

--- client-app/app/src/main/java/org/feup/group4/supermarket/service/UserService.kt
+++ client-app/app/src/main/java/org/feup/group4/supermarket/service/UserService.kt
@@ -3,8 +3,23 @@ package org.feup.group4.supermarket.service
 import android.content.Context
 import org.feup.group4.supermarket.model.User
 
-class UserService(context: Context, afterRequest: AfterRequest?) : HttpService(context, afterRequest) {
-    fun getUser() = get("/user")
+class UserService(
+    context: Context,
+    afterRequest: AfterRequest?,
+    afterConnectException: (() -> Unit)? = null
+) : HttpService(context, afterRequest, afterConnectException) {
+    fun getUserFromServer() = get("/user")
+
+    fun getUserFromStorage() = run {
+        val sharedPref = context.getSharedPreferences("user", Context.MODE_PRIVATE)
+        User(
+            nickname = sharedPref.getString("nickname", null) ?: "",
+            name = sharedPref.getString("name", null),
+            accumulated_value = sharedPref.getFloat("accumulated_value", 0f).toDouble(),
+            is_admin = sharedPref.getBoolean("is_admin", false),
+            password = ""
+        )
+    }
 
     fun saveUser(user: User) {
         val sharedPref = context.getSharedPreferences("user", Context.MODE_PRIVATE)
@@ -12,6 +27,7 @@ class UserService(context: Context, afterRequest: AfterRequest?) : HttpService(c
             putString("nickname", user.nickname)
             putString("name", user.name)
             putFloat("accumulated_value", user.accumulated_value?.toFloat() ?: 0f)
+            putBoolean("is_admin", user.is_admin ?: false)
             commit()
         }
     }
